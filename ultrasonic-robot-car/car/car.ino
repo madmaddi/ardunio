1197f2c749f5057ab99ce41533486af0591fcb98
madmaddi <mopmop19@freenet.de>
2016-09-04 23:58:23 +0200

    refactor

--- ultrasonic-robot-car/car/car.ino
+++ ultrasonic-robot-car/car/car.ino
@@ -1,29 +1,25 @@
-/* Code Written by Roy Pe'er. I've explained all the code in the grey comments.
-I recommend you to go over the code, examine it, play with it, improve it and modify it according to your needs. 
-For more awesome videos, subsrice to my channel:
-http://www.youtube.com/subscription_center?add_user=Roypeer1
-*/
 
-#include <AFMotor.h> //import your motor shield library
-#define trigPin 12 // define the pins of your sensor
+#include <AFMotor.h> 
+#define trigPin 12 
 #define echoPin 13 
-//AF_DCMotor motor1(1,MOTOR12_64KHZ); // set up motors.
-AF_DCMotor motor1(1, MOTOR12_64KHZ); // set up motors.
+
+AF_DCMotor motor1(1, MOTOR12_64KHZ);
 AF_DCMotor motor2(2, MOTOR12_64KHZ);
 
-// Benoetigte Variablen werden definiert
-int maximumRange = 300; 
-int minimumRange = 2; 
-
-long abstand;
-long dauer;
-long distance;
 bool motorEnabled = true;
 
 int delayTime = 250;
 int turnOverTime = 500;
 int waitTime = 500;
 int forwardTime = 1000;
+
+int carspeed2 = 200;
+int carspeed1 = carspeed2 - 8;
+
+long abstand;
+long dauer;
+long distance;
+
 /*
   * 0 = STOP
   * 1 = FORWARD
@@ -33,38 +29,27 @@ int forwardTime = 1000;
 */
 int motorStatus = 0 ;
 
-int carspeed2 = 200;
-int carspeed1 = carspeed2 - 8;
-
-
 void setup() {
-  
   Serial.begin(9600); // begin serial communitication  
-  Serial.println("Motor test!");
-  pinMode(trigPin, OUTPUT);// set the trig pin to output (Send sound waves)
-  pinMode(echoPin, INPUT);// set the echo pin to input (recieve sound waves)
-
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   setMotorSpeed(carspeed1, carspeed2); //set the speed of the motors, between 0-255
-
-  motor1.run(RELEASE);
+  motor1.run(RELEASE); // stop motors
   motor2.run(RELEASE);
 }
 
 void loop() {
   delay(waitTime);
   distance = cumulateDistance();
-  //Serial.println(distance);
   
   if ( distance < 40.0 ) { 
     Serial.println(distance);  
     // 1st.: STOP
     stopMotor();  
     
-    // change direction unless distance greater than 0.4m
+    // change direction unless distance lower than x
     while(distance < 60.0 ) {
-      distance = cumulateDistance();
-      //Serial.println(distance);
-      //Serial.println("turn over");  
+      distance = cumulateDistance();  
       delay(waitTime);
       turnLeft();
       delay(turnOverTime);
@@ -75,12 +60,11 @@ void loop() {
     }
   } else {
     forwardMotor();
-    delay(forwardTime); // drive through 0.5s
+    delay(forwardTime); // drive through some ms
     stopMotor();
   }
 
   Serial.println("--------------");
-  
 }
 
 long cumulateDistance(){
@@ -90,17 +74,14 @@ long cumulateDistance(){
   long di[5];
   long avg = 0.0;
   long distance = 0.0;
-//  Serial.println("Distance:");
+  
   for (int i=0; i<measurements; i++){
     distance = fetchDistance();
-//    Serial.println(distance);
     avg += distance;
     di[i] = distance;
   }
   
   avg = avg/measurements;
-  //Serial.println("avg: ");
-  //Serial.println(avg);
   
   // get lowest distance from di to avg
   long avgDistance = 0.0;
@@ -113,7 +94,7 @@ long cumulateDistance(){
       returnValue = di[i];
     }
   }
-  //Serial.println("Distance");
+  
   Serial.println(returnValue);
   return returnValue;
 }
@@ -123,7 +104,6 @@ void turnLeft(){
   if ( motorStatus != 3 ) {
     Serial.println("turn left");  
     motorStatus = 3;
-    //setMotorSpeed(50);
     if ( !motorEnabled ) return;
     motor1.run(FORWARD);
     motor2.run(BACKWARD);
@@ -162,9 +142,6 @@ void forwardMotor() {
 }
 
 void setMotorSpeed(int sspeed1, int sspeed2) {
-  //Serial.println("setMotorSpeed");
-  //Serial.println(sspeed);
-  delay(10);
   motor1.setSpeed(sspeed1); //set the speed of the motors, between 0-255
   motor2.setSpeed(sspeed2); 
 }
